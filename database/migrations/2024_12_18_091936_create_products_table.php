840871c026ad089db19a6b0faff5e0ff5982032d
Umaima290 <umaimaaijaz456@gmail.com>
2024-12-18 17:39:48 +0500

    tables,folders and file

--- database/migrations/2024_12_18_091936_create_products_table.php
+++ database/migrations/2024_12_18_091936_create_products_table.php
@@ -14,18 +14,15 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name', 150); // Product name
+            $table->string('image');
             $table->text('description')->nullable(); // Product description
-            $table->unsignedBigInteger('category_id'); // Foreign key for product category
             $table->decimal('price', 10, 2)->default(0.00); // Product price
             $table->integer('quantity')->default(0); // Quantity in stock
             $table->enum('status', ['Available', 'Out of Stock', 'Discontinued'])->default('Available'); // Product status
-            $table->timestamps(); // created_at and updated_at columns
-
             // Foreign key constraint
-            $table->foreign('category_id')
-                  ->references('category_id') // Reference to product_categories table
-                  ->on('product_categories')
-                  ->onDelete('cascade'); // Deletes products when the category is deleted
+            $table->foreignId('category_id')
+                  ->constrained()
+                  ->cascadeOnDelete(); // Deletes products when the category is deleted
 
             $table->timestamps();
         });
